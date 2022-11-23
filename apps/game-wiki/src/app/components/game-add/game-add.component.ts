280f7a1a1a1d01065a4c56137fe8202dfebb16e0
Luca.R04 <Lucarinck04@gmail.com>
2022-11-23 17:49:10 +0100

    working create

--- apps/game-wiki/src/app/components/game-add/game-add.component.ts
+++ apps/game-wiki/src/app/components/game-add/game-add.component.ts
@@ -1,4 +1,8 @@
+/* eslint-disable @angular-eslint/no-empty-lifecycle-method */
+/* eslint-disable @typescript-eslint/no-empty-function */
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { GameService } from '../../services/game.service';
 
 @Component({
   selector: 'game-wiki-game-add',
@@ -6,7 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-add.component.css'],
 })
 export class GameAddComponent implements OnInit {
-  constructor() {}
+  constructor(private gameService: GameService, private formBuilder: FormBuilder,) {}
+
+  createForm = this.formBuilder.group({
+    name: '',
+    description: '',
+    price: '',
+    category: '',
+    image: '',
+    releaseDate: ''
+  });
+
+  onSubmit(): void {
+    console.log(this.createForm.value)
+
+    this.gameService.addGame(this.createForm.value);
+  }
 
   ngOnInit(): void {}
 }
