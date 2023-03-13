397c44325214664cf7a2ec5225837cbf63879428
Luca.R04 <Lucarinck04@gmail.com>
2023-03-13 14:11:28 +0100

    updated formatting

--- apps/api/src/constants.ts
+++ apps/api/src/constants.ts
@@ -1,7 +1,5 @@
 export const MONGO_CONNECTION = 'mongodb+srv://nestjs-admin:dxgEPworUUsP5rFu@game-wiki.1sfgx4f.mongodb.net/game-wikiDB?retryWrites=true&w=majority';
-
 export const JWT_SECRET = "D6QKyPskVNoP10R"
-
 export const NEO4J_URI = "b34efc16.databases.neo4j.io"
 export const NEO4J_USERNAME = "neo4j"
 export const NEO4J_PWD = "HV46BKmZlnhC-xRo5U1-VSmylLTer0SE9HpUV7hvk9s"
